757b72b9e0def52cfc91541796555cde39a287d6
unknown <matheus.duartesilva42@gmail.com>
2023-09-07 19:32:32 -0300

    desenvolvimento do header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Dictionary from "./components/Dictionary/Dictionary"
 import Header from "./components/Header/Header"
 
 const App = () => {
   return (
     <div>
       <Header />
+      <Dictionary />
     </div>
   )
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,21 @@
+import { PiBookBookmarkThin } from 'react-icons/pi'
+import { BsMoon } from 'react-icons/bs'
 import './Header.css'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className='header'>
+      <div className="left">
+        <PiBookBookmarkThin className='icon book' />
+      </div>
+
+      <div className="right">
+        <label>
+          <input type="checkbox" className='toggle' />
+        </label>
+        <BsMoon className='icon moon' />
+      </div>
+    </header>
   )
 }
 
